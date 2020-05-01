eefafd78a310d855542f9bff2acb25cb2285a7e7
sabrina banks <sabrinabanks@sabrinas-MacBook-Pro.local>
2020-05-01 01:45:51 -0700

    Updates

---
+++ precache-manifest.608ce5cf9ec0bc44150209beacbcb17b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85075f7bba725b160edf29d4db687727",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97b46770c71acb906b68",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "10f49d9e54d98300413a",
+    "url": "/static/css/main.dbba2ee8.chunk.css"
+  },
+  {
+    "revision": "97b46770c71acb906b68",
+    "url": "/static/js/2.4f830a65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4f830a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f49d9e54d98300413a",
+    "url": "/static/js/main.e6576114.chunk.js"
+  },
+  {
+    "revision": "c48fe6f5746d7cf17d40",
+    "url": "/static/js/runtime-main.e41dc4ae.js"
+  },
+  {
+    "revision": "3f47f28837408e26c8a169383c957aa6",
+    "url": "/static/media/Sabrinaresume.3f47f288.png"
+  },
+  {
+    "revision": "85d90c735e74a83324d224aa8ed08506",
+    "url": "/static/media/adobe.85d90c73.svg"
+  },
+  {
+    "revision": "fb07e614d24f4175c5ed5a6bcd643d04",
+    "url": "/static/media/bitbubble.fb07e614.svg"
+  },
+  {
+    "revision": "a1897030d8d704a3f70c8285bc4fb72b",
+    "url": "/static/media/bitfolder1.a1897030.svg"
+  },
+  {
+    "revision": "68e4f8eddd4a34f73806dd95818a6838",
+    "url": "/static/media/bithandicon.68e4f8ed.svg"
+  },
+  {
+    "revision": "e38d24180ec33413c11a8a70152f079b",
+    "url": "/static/media/bitheart.e38d2418.svg"
+  },
+  {
+    "revision": "3681c75d267409b26668fa4e78673c5a",
+    "url": "/static/media/bitheartwhite.3681c75d.svg"
+  },
+  {
+    "revision": "ecbae03a1af0d167de616a8bde66532f",
+    "url": "/static/media/bitmail.ecbae03a.svg"
+  },
+  {
+    "revision": "a7de79cc947e9d66825bd6a24c78945d",
+    "url": "/static/media/bitname.a7de79cc.svg"
+  },
+  {
+    "revision": "1d6f0a6e02c666fdd0faa2c3a9e51d07",
+    "url": "/static/media/cross1.1d6f0a6e.png"
+  },
+  {
+    "revision": "3ca17aa02157af9a65a5412ae28f1bd9",
+    "url": "/static/media/ember.3ca17aa0.svg"
+  },
+  {
+    "revision": "05ef54291d148de488c62b00ee2c6667",
+    "url": "/static/media/experience.05ef5429.svg"
+  },
+  {
+    "revision": "a2274ff35f69a17a9d5d603c56f07b40",
+    "url": "/static/media/github.a2274ff3.svg"
+  },
+  {
+    "revision": "1931a81400d1b976ad187b3e68829f78",
+    "url": "/static/media/githubline.1931a814.svg"
+  },
+  {
+    "revision": "230a264a1fd554ebb935f055e0ac26c3",
+    "url": "/static/media/instagram.230a264a.svg"
+  },
+  {
+    "revision": "fca64e78576731126220a6a7048108b0",
+    "url": "/static/media/javaicon.fca64e78.svg"
+  },
+  {
+    "revision": "79cb279bad6e4858e029af6e668aa292",
+    "url": "/static/media/lightroom.79cb279b.svg"
+  },
+  {
+    "revision": "9f95f193e2bb4caa7cef144874d38cb2",
+    "url": "/static/media/linkedin.9f95f193.svg"
+  },
+  {
+    "revision": "43cf2efcae64de253b48a5ab144b3003",
+    "url": "/static/media/logo.43cf2efc.svg"
+  },
+  {
+    "revision": "692c37377cd1e0f4850f47c7c0148426",
+    "url": "/static/media/monofont1.692c3737.ttf"
+  },
+  {
+    "revision": "de7666806aa17afade8ecc7662ac36a5",
+    "url": "/static/media/phoenix.de766680.svg"
+  },
+  {
+    "revision": "c495f9a3702d2085aa29294eca4a591c",
+    "url": "/static/media/photoshop.c495f9a3.svg"
+  },
+  {
+    "revision": "d15736bc2759eaa4d87ecf4bde850104",
+    "url": "/static/media/postgresqlicon.d15736bc.svg"
+  },
+  {
+    "revision": "3585fb3269c9bc3c3a61314b43e1371a",
+    "url": "/static/media/rubyicon.3585fb32.svg"
+  },
+  {
+    "revision": "e5ad0e783287e66f138c1eec1c46df24",
+    "url": "/static/media/sabrinajumboname.e5ad0e78.svg"
+  }
+]);
